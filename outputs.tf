2b7c28c9342b47ce6995dddd52ac271b11ff6c59
skesarkar <skesarkar@schubergphilis.com>
2024-06-17 23:00:20 +0200

    enhancement: output cloudwatch log group name and arn

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "cloudwatch_log_group_arn" {
+  description = "Map of CloudWatch log group type and arn when atleast one log type is enabled"
+  value       = { for log_type, log_configuration in var.cloudwatch_logging_configuration : log_type => aws_cloudwatch_log_group.default[log_type].arn if log_configuration.enabled }
+}
+
+output "cloudwatch_log_group_name" {
+  description = "Map of CloudWatch log group type and name when atleast one log type is enabled"
+  value       = { for log_type, log_configuration in var.cloudwatch_logging_configuration : log_type => aws_cloudwatch_log_group.default[log_type].name if log_configuration.enabled }
+}
+
 output "firewall_status" {
   description = "Information about the current status of the firewall."
   value       = aws_networkfirewall_firewall.default.firewall_status
